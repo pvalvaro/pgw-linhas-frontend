409e0d42552f9861924da932fdc98f197376fa23
pvalvaro <pvalvaro25@gmail.com>
2023-08-28 08:53:04 -0300

    retificacao 3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4200;
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(__dirname + '/dist/pgw-linhas-frontend'));
