ac9fa0c9947bc15a315caa765e84f3a5c4045036
pvalvaro <pvalvaro25@gmail.com>
2023-08-28 07:41:16 -0300

    Preparação do projeto para heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+//Importa as dependências que acabamos de instalar
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static("./dist/pgw-linhas-frontend"));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/pgw-linhas-frontend/'}),
+);
+
+// Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 8080);
